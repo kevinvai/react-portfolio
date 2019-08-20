251fc83bf26e6cacb89bbfc6884cf962aca3f5ba
kevinvai10 <kevin.cbdev@gmail.com>
2019-08-20 00:01:48 -0700

    Updates

---
+++ precache-manifest.343164864ac91400ca3ce7a68feb468c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "98df80d3ce171a30223b",
+    "url": "/react-portfolio/static/js/main.98df80d3.chunk.js"
+  },
+  {
+    "revision": "591664a1c0dde4f94f41",
+    "url": "/react-portfolio/static/js/1.591664a1.chunk.js"
+  },
+  {
+    "revision": "98df80d3ce171a30223b",
+    "url": "/react-portfolio/static/css/main.ad166b93.chunk.css"
+  },
+  {
+    "revision": "05774c02bd42382bc3d91c941118e786",
+    "url": "/react-portfolio/index.html"
+  }
+];
